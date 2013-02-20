f7075882b8acfb0b8080b5c096e4ef422164eace
Dustin McCraw <dustin@lightspeedsystems.com>
2013-02-20 08:15:49 -0800

    remove logger code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_time_zone
 
   rescue_from CanCan::AccessDenied do |exception|
-    Rails.logger.red("Access denied on #{exception.action} #{exception.subject.inspect}")
+    # Rails.logger.red("Access denied on #{exception.action} #{exception.subject.inspect}")
     flash[:error] = exception.message
     redirect_to root_path
   end
