1a108afe02a9b28d50aa03b76fa59a78f06bb888
Dustin McCraw <dustin@lightspeedsystems.com>
2015-03-13 22:25:23 -0700

    update to rails 4.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
   before_filter :authenticate_user!
   before_filter :set_time_zone
   before_filter :set_locale
@@ -14,11 +12,13 @@ class ApplicationController < ActionController::Base
   private
 
   def set_time_zone
-    Time.zone = (current_user ? current_user.time_zone : nil) || ClassroomEconomy::Application.config.time_zone
+    Time.zone = (current_user ? current_user.time_zone : nil) || Rails.application.config.time_zone
   end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-
+    # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 end
