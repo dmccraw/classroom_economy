3bd94a8ea3813bceb882faa8a4126dec4df544db
Dustin McCraw <dustin.mccraw@teamsnap.com>
2018-07-29 10:41:42 -0700

    Needed to handle strong params for devise
    https://github.com/plataformatec/devise/tree/3-stable#strong-parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_filter :authenticate_user!
   before_filter :set_time_zone
   before_filter :set_locale
@@ -21,4 +22,11 @@ class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_type, :first_name, :last_name, :email, :password, :password_confirmation) }
+  end
+
 end
